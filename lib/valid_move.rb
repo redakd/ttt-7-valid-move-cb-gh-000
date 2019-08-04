04e9dc8c1d065d60ad0760095a76feadeff5ff95
Rida Kodad <kodadreda@gmail.com>
2019-08-04 14:57:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,7 @@
 def valid_move?(number)
   if position_taken?
   end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if board[index] == ' ' or board[index] == ''
